09e4c69042db22f6b180ddad3106a24cc95932d4
nspencerjackson <nspencerjackson@gmail.com>
2022-01-19 16:42:01 +0800

    testing out tkinter

---
+++ tk_test.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+
+root = tk.Tk()
+root.title("Tkinter Open File Dialog")
+root.resizable(False, False)
+root.geometry("300x150")
+
+def select_file():
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All Files', '*.*')
+    )
+
+    filename = fd.askopenfilename(
+        title = 'Open a file',
+        initialdir = "/",
+        filetypes = filetypes)
+
+    showinfo(
+        title = "Selected File",
+        message = filename
+    )
+
+open_button = ttk.Button(
+    root,
+    text = "Open",
+    command = select_file
+)
+
+open_button.pack(expand = True)
+
+root.mainloop()
